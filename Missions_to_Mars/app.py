eacd8a613e0a53185306a0009ff36d349dd94a30
Andrew Baker <abaker178@gmail.com>
2021-02-20 10:15:29 -0500

    Flask app complete

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,11 +1,28 @@
+# Import Dependencies
 from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
 
-
+# Initialize App
 app = Flask(__name__)
 
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_db"
+mongo = PyMongo(app)
+
+#### ROUTES ####
 @app.route("/")
 def echo():
-    return render_template("index.html")
+    data = mongo.db.data.find_one()
+    return render_template("index.html", data=data)
+
+@app.route("/scrape")
+def scrape():
+    mars_data = mongo.db.data
+    new_data = scrape_mars.scrape()
+    mars_data.update({}, new_data, upsert=True)
+    return redirect("/", code=302)
+#### END ROUTES ####
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ Missions_to_Mars/scrape_mars.py
@@ -0,0 +1,83 @@
+# Import Dependencies
+from bs4 import BeautifulSoup as bs
+from splinter import Browser
+import pandas as pd
+from webdriver_manager.chrome import ChromeDriverManager
+
+def scrape():
+    # Initialize browser
+    def init_browser():
+        executable_path = {'executable_path': ChromeDriverManager().install()}
+        return Browser('chrome', **executable_path, headless=False)
+
+    # Function to scrape websites
+    ## INPUT: URL as a string
+    ## OUTPUT: BeautifulSoup object with html from the scraped website
+    def scrapeHTML(url):
+        browser.visit(url)
+        return bs(browser.html, "html.parser")
+
+    browser = init_browser()
+
+    #### NASA ####
+    # Scrape NASA Mars website
+    nasa_url = "https://mars.nasa.gov/news/"
+    nasa_soup = scrapeHTML(nasa_url)
+    # Get the news articles list and store the first (latest) article's information
+    first_article = nasa_soup.find("div", class_="list_text")
+    news_title = first_article.find("div", class_="content_title").text
+    news_p = first_article.find("div", class_="article_teaser_body").text
+
+    #### JPL ####
+    # Scrape JPL
+    jpl_url = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html"
+    jpl_soup = scrapeHTML(jpl_url)
+    # Capture the featured image on the JPL site
+    rel_path = jpl_soup.find("img", class_="headerimage")["src"]
+    featured_image_url = jpl_url + "/" + rel_path
+
+    #### Mars Facts ####
+    # Scrape Mars Fact site tables
+    facts_url = "https://space-facts.com/mars/"
+    facts_tables = pd.read_html(facts_url)
+    # Convert each to HTML and store the list
+    facts_tables_html = []
+    for table in facts_tables:
+        facts_tables_html.append(table.to_html())
+
+    #### USGS Astrogeology ####
+    # Scrape USGS Astrogeology
+    usgs_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    usgs_soup = scrapeHTML(usgs_url)
+    # Capture links to each hemisphere page
+    item_list = usgs_soup.find_all("a", class_="itemLink")
+    title_list = []
+    for item in item_list:
+        if item.text not in title_list and len(item.text) > 1:
+            title_list.append(item.text)
+    # Navigate to each hemisphere page, scrape it, and store the full size image url
+    img_list = []
+    for link in title_list:
+        browser.links.find_by_partial_text(link).click()
+        img = browser.find_by_css(".wide-image")["src"]
+        img_list.append(img)
+        browser.visit(usgs_url) # return to the main page
+    # Iterate through the titles and image urls and create a dictionary for each
+    hemisphere_image_urls = []
+    for (t, i) in zip(title_list, img_list):
+        hemisphere_image_urls.append({
+            "title": t,
+            "img_url": i
+        })
+
+    #### Package and return data dictionary  ####
+    data_dict = {
+        "news_title": news_title,
+        "news_p": news_p,
+        "featured_image_url": featured_image_url,
+        "facts_tables_html": facts_tables_html,
+        "hemisphere_image_urls": hemisphere_image_urls
+    }
+    browser.quit()
+
+    return data_dict
